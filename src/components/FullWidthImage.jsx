cb1ca95546b20db8b7136dd588a4be2edd7a0594
aziz rebhi <mohamedaziz.rebhi@ensi-uma.tn>
2025-06-30 23:30:12 +0100

    replaced photo by supabase storage

--- src/components/FullWidthImage.jsx
+++ src/components/FullWidthImage.jsx
@@ -1,14 +1,16 @@
-import ARE from "../assets/ARE.jpg";
+import React from "react";
 
 const FullWidthImage = () => {
+  const imageUrl =
+    "https://rmjmjkuptvhlzgojemkt.supabase.co/storage/v1/object/public/photos//ARE.jpg";
+
   return (
     <div className="relative w-full">
       <img
-        src={ARE}
+        src={imageUrl}
         alt="Robotics Club"
         className="w-full h-auto object-cover"
       />
-
       <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40">
         <h1
           className="text-white text-3xl sm:text-5xl lg:text-6xl text-center animate-fade-in-slow"
